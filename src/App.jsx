c1b40652d38b99f6c534e6099d4a2b1c21e0bed5
Luis Gustavo Polio Fernández <guspolio@outlook.es>
2023-12-17 01:29:18 -0600

    axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import "./App.css"
+import axios from "axios";
 
 function App() {
+  const API_URL = import.meta.env.VITE_API_URL;
+  const [criptomonedas, setcriptomonedas] = useState();
+
+  useEffect(() => {
+    axios.get(`${API_URL}assets`)
+      .then((data) => {
+        console.log(data);
+        setcriptomonedas(data.data.data);
+      })
+      .catch(() => {
+        console.error("operación fallida");
+      });
+  }, []);
+  if (!criptomonedas) return <span>Cargando...</span>;
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <h1>Lista criptomonedas</h1>
+
+      <ol>
+        {criptomonedas.map(({id,name, priceUsd, symbol}) => (
+          <li key={id}>
+            Nombre: {name}, Precio: {priceUsd},
+            Simbolo: {symbol}
+          </li>
+        ))}
+      </ol>
+    </>
+  );
 }
 
-export default App
+export default App;
